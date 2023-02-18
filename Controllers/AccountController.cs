40545d9280765b977fe8fcda3966760c956c7b5e
nileshraut123 <76642697+nileshraut123@users.noreply.github.com>
2023-02-18 15:32:44 +0530

    Initail 1 commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using System.Web;
+using System.Web.Security;
+
+namespace Transactiondetails.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string LogOut()
+        {
+        
+
+            var requestContext = HttpContext.Current.Request.RequestContext;
+            return   new System.Web.Mvc.UrlHelper(requestContext).Action("Login", "Account");
+        }
+    }
+}
