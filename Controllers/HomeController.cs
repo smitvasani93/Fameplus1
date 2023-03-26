d6e2c8bea2c1e5c31a5e205467f924d56a1cd064
KetanPatel <mr.ketanpatel@gmail.com>
2023-03-26 16:39:10 +0530

    Revert "Db changes for Edit JobWorkRecipt"
    This reverts commit 0da35d33a85cca23e02f0b63a2f1ae6c93da48d6.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -41,10 +41,10 @@ namespace Transactiondetails.Controllers
                 var userData = (UserData)Session["UserData"];
 
                 var jobReciept = dbutility.GetJobReciept(userData.Company, userData.FYear);
-                var recieptNo = jobReciept.JobRecieptMasts.Max(a => a.SerialNumber);
+                var recieptNo = jobReciept.JobReciepts.Max(a => a.SerialNumber);
                 recieptNo++;
                 TempData["recieptNo"] = recieptNo;
-                ViewBag.Search = jobReciept.JobRecieptMasts;
+                ViewBag.Search = jobReciept.JobReciepts;
                 ViewBag.Process = jobReciept.Processes;
                 ViewBag.Customer = jobReciept.Accounts;
             }
@@ -58,35 +58,21 @@ namespace Transactiondetails.Controllers
         [HttpPost]
         public PartialViewResult EditJobworkReceipt(int serialNo)
         {
-            var dbutility = new JobReceiptDataLayer();
-
-            try
+            using (TransactionDetailsEntities db = new TransactionDetailsEntities())
             {
-                var userData = (UserData)Session["UserData"];
-
-                var jobReciept = dbutility.GetJobRecieptBySerialNumber(userData.Company, userData.FYear, serialNo);
-
-                TempData["AccountCode"] = jobReciept.JobRecieptDets.FirstOrDefault().AccountCode;
-                TempData["ReferenceDate"] = Convert.ToDateTime(jobReciept.JobRecieptDets.FirstOrDefault().ReferenceDate).ToString("yyyy-MM-dd");  //Convert.ToDateTime(db.JobReceiptMas.Where(x => x.SerialNumber == serialNo).FirstOrDefault().ReferenceDate).ToString("yyyy-MM-dd");
-
-                ViewBag.Process = jobReciept.Processes;
-                ViewBag.Customer = jobReciept.Accounts;
+                List<sp_GetSelectedData_Result> lstSelectedData = new List<sp_GetSelectedData_Result>();
+                lstSelectedData = db.sp_GetSelectedData(serialNo).ToList();
+                ViewBag.Data = lstSelectedData;
+                var processList = db.ProcessMas.Select(a => new { a.ProcessCode, a.ProcessName }).ToList();
+                var customerList = db.Accounts.Select(a => new { a.AccountCode, a.AccountName }).ToList();
+                TempData["AccountCode"] = db.JobReceiptMas.Where(x => x.SerialNumber == serialNo).FirstOrDefault().AccountCode;
+                TempData["ReferenceDate"] = Convert.ToDateTime(db.JobReceiptMas.Where(x => x.SerialNumber == serialNo).FirstOrDefault().ReferenceDate).ToString("yyyy-MM-dd");
+                ViewBag.Process = processList;
+                ViewBag.Customer = customerList;
                 TempData["serialNo"] = serialNo;
                 TempData.Keep("serialNo");
-                return PartialView(jobReciept.JobRecieptDets);
-
-                 
+                return PartialView(lstSelectedData);
             }
-            catch (Exception ex)
-            {
-            }
-
-            //ViewBag.Process = processList;
-            //ViewBag.Customer = customerList;
-            //TempData["serialNo"] = serialNo;
-            //TempData.Keep("serialNo");
-            return PartialView();
-             
         }
         public JsonResult SaveJobworkReceipt(List<JobReceiptDet> lstjobReceiptDets, string referenceDate = "", string accountCode = "", int serialNumber = 0)
         {
--- Models/Utility/JobReceiptDataLayer.cs
+++ Models/Utility/JobReceiptDataLayer.cs
@@ -12,6 +12,9 @@ namespace Transactiondetails.Models.Utility
         public JobRecieptData GetJobReciept(string companyCode, string FYear)
         {
             var JobRecieptData = new JobRecieptData();
+            var processMasterList = new List<ProcessMaster>();
+            var accountMasterList = new List<AccountMaster>();
+            var jobRecieptMasterList = new List<JobRecieptMaster>();
             using (CompanyDBContext db = new CompanyDBContext(companyCode))
             {
                 //Call Stored Procedure to get the JobReciepts
@@ -19,24 +22,7 @@ namespace Transactiondetails.Models.Utility
                 JobRecieptData.Accounts = db.Database.SqlQuery<AccountMaster>("exec spGetAccount").ToList();
                 var pFYear = new SqlParameter("@FinancialYearCode", FYear);
 
-                JobRecieptData.JobRecieptMasts = db.Database.SqlQuery<JobRecieptMaster>("exec spGetJobReceipt @FinancialYearCode", pFYear).ToList();
-            }
-
-            return JobRecieptData;
-        }
-        public JobRecieptData GetJobRecieptBySerialNumber(string companyCode, string FYear, int serialNo)
-        {
-            var JobRecieptData = new JobRecieptData();
-             
-
-            using (CompanyDBContext db = new CompanyDBContext(companyCode))
-            {
-                //Call Stored Procedure to get the JobReciepts
-                JobRecieptData.Processes = db.Database.SqlQuery<ProcessMaster>("exec spGetProcessMaster").ToList();
-                JobRecieptData.Accounts = db.Database.SqlQuery<AccountMaster>("exec spGetAccount").ToList();
-                var pSNumber = new SqlParameter("@SerialNumber", serialNo);
-
-                JobRecieptData.JobRecieptDets = db.Database.SqlQuery<JobRecieptDetail>("exec SpGetJobRecieptBySerialNumber @SerialNumber", pSNumber).ToList();
+                JobRecieptData.JobReciepts = db.Database.SqlQuery<JobRecieptMaster>("exec spGetJobReceipt @FinancialYearCode", pFYear).ToList();
             }
 
             return JobRecieptData;
